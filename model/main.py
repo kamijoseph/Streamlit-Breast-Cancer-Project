99e7f2e39c71bb6dc2035fd86b7b96d9a58745d7
Kami <josephkiarie561@gmail.com>
2025-07-30 11:43:55 +0300

    Scaling. Splitting and Training

--- model/main.py
+++ model/main.py
@@ -2,6 +2,29 @@
 # Training the Logistic regression model
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+# Model function
+def build_model(data):
+    # separating X feature and y labels
+    X, y = data.drop(["diagnosis"], axis=1), data["diagnosis"]
+
+    # scaling the features
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
+    # splitting the data into training and testing data
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        test_size=0.2,
+        random_state=21
+    )
+
+    # training the logistic regression model
+    log_reg = LogisticRegression()
+    log_reg.fit(X_train, y_train)
 
 # data cleaning function
 def cleaned_data():
@@ -19,6 +42,8 @@ def cleaned_data():
 def main():
     data = cleaned_data()
     print(data.head())
+
+
 
 
 
